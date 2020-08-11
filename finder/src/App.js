e6163d8ad1185b153d815be36ffb2403940a8a20
haihuanchen <haihuan.chen@hotmail.com>
2020-08-11 15:33:05 -0400

    setup files with create-react-app

---
+++ finder/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Build The Finder App</h1>
+      {/* Header */}
+      {/* Finder Cards */}
+      {/* Buttons below finder cards */}
+      {/* Chats Screen */}
+    </div>
+  );
+}
+
+export default App;
