cc76a6845557e62f386658c756dd4a0dfbe4f389
haihuanchen <haihuan.chen@hotmail.com>
2020-08-13 17:43:39 -0400

    added chats and routes to chat

--- finder/src/App.js
+++ finder/src/App.js
@@ -4,20 +4,21 @@ import Header from './Header'
 import FinderCards from './FinderCards'
 import SwipeButton from './SwipeButton'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ChatsContainer from './ChatsContainer';
 
 function App() {
   return (
     <div className="app">
     <Router>
-    <Header />
       <Switch>
         <Route exact path='/'>
-          <h1>The Finder App Main Page</h1>
+          <Header />
           <FinderCards />
           <SwipeButton />
         </Route>
         <Route path="/chat">
-          <h1>The Finder App Chat Page</h1>
+          <Header backButton="/"/>
+          <ChatsContainer />
         </Route>
       </Switch>
     </Router>
