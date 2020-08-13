ed9d815bac2a6bb0b6ee4e90163ce7e8556ff54b
haihuanchen <haihuan.chen@hotmail.com>
2020-08-13 13:25:36 -0400

    added swipe buttons and links

--- finder/src/Header.js
+++ finder/src/Header.js
@@ -3,6 +3,7 @@ import "./Header.css"
 import PersonIcon from '@material-ui/icons/Person';
 import MessageIcon from '@material-ui/icons/Message';
 import IconButton from '@material-ui/core/IconButton';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return(
@@ -10,13 +11,17 @@ const Header = () => {
             <IconButton>
                 <PersonIcon className="header-icon" fontSize="large"/>
             </IconButton>
-            <img className="header-logo"
-            src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/fire.png"
-            alt="finder logo"
-            />
-            <IconButton>
-                <MessageIcon className="header-icon" fontSize="large"/>
-            </IconButton>
+            <Link to="/">
+                <img className="header-logo"
+                src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/fire.png"
+                alt="finder logo"
+                />
+            </Link>
+            <Link to="/chat">
+                <IconButton>
+                    <MessageIcon className="header-icon" fontSize="large"/>
+                </IconButton>
+            </Link>
         </div>
     )
 }
